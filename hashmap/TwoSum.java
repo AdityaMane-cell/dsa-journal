2e903a3a44e16b9281a78b42a2569f37ae6ccb25
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-06-23 17:42:19 +0530

    add: solved leetcode problem 1

---
+++ hashmap/TwoSum.java
@@ -0,0 +1,31 @@
+package hashmap;
+/*
+ * Problem: Two Sum
+ * Difficulty: Easy
+ * Topic: Hashmap
+ * Pattern: Complement-lookup
+ *
+ * Approach:
+ * Use a hashmap to store value->index as we iterate.
+ * For each number, check if (target - num) already exists.
+ *
+ * Time:  O(n)
+ * Space: O(n)
+ * 
+ */
+
+import java.util.HashMap;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
