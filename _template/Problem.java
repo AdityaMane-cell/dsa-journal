e1644968a0f7c367eb816ae9b75d36a9927bc374
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-07-01 09:03:19 +0530

    update: updating problem template

--- _template/Problem.java
+++ _template/Problem.java
@@ -1,30 +1,17 @@
 /*
- * Problem: Two Sum
- * Difficulty: Easy
- * Topic: Hashmap
- * Pattern: Complement-lookup
+ * Problem: 
+ * Difficulty: 
+ * Topic: 
+ * Pattern: 
  *
  * Approach:
- * Use a hashmap to store value->index as we iterate.
- * For each number, check if (target - num) already exists.
+ * 
  *
- * Time:  O(n)
- * Space: O(n)
+ * Time:  
+ * Space: 
  * 
  */
 
-import java.util.HashMap;
-
-class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
-        }
-        return new int[] {};
-    }
+class Problem {
+    // here goes solution
 }
